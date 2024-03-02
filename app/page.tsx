094ef65bdd53489278f616286c14bf56b0ba5803
eddyc0de <edgar.pedreno@gmail.com>
2024-03-02 15:50:39 +0100

    Seed database

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,9 @@ export default function Page() {
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Add Hero Images Here */}
           <Image
             src="/hero-mobile.png"
             width={560}
